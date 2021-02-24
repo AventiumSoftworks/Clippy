2868b6b18f30344bf64130ec13a00a5d2d48a552
GeekCornerGH <45696571+GeekCornerGH@users.noreply.github.com>
2021-02-24 09:27:48 +0100

    Ajout d'une ligne

--- src/commandes/crackfr.js
+++ src/commandes/crackfr.js
@@ -10,7 +10,7 @@ module.exports = {
             .setColor('BLURPLE')
             .setTitle('404 - Not found')
             .setDescription("Helios Launcher ne supporte pas les versions crackées de Minecraft. Et elles ne seront jamais supportées.")
-            .addField("Les versions crackées sont interdites.", "Quand vous jouez à Minecraft, vous acceptez les [Conditions de Mojang](https://account.mojang.com/terms), qui stipulent que vous devez obtenir une copie légale de Minecraft pour y jouer.")
+            .addField("Les versions crackées sont interdites.", "Quand vous jouez à Minecraft, vous acceptez les [Conditions de Mojang](https://account.mojang.com/terms), qui stipulent que vous devez obtenir une copie légale de Minecraft pour y jouer.\nN'espérez pas obtenir de l'aide concernant les cracks.")
             .setTimestamp()
             .setFooter(`Demandé par ${message.author.tag}`, message.author.displayAvatarURL())
         message.channel.send(crack)
