4e7625ec1c30f947cf72b34fa6d02e36d5a059ff
GeekCornerGH <45696571+GeekCornerGH@users.noreply.github.com>
2021-03-12 16:32:42 +0100

    Ajout de l'event Message sur le handler

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const fs = require('fs-extra');
 // Handler Commandes
 client.commands = new Discord.Collection();
 const cooldowns = new Discord.Collection();
+client.cooldowns = cooldowns
 
 const commandfiles = fs.readdirSync('./src/commandes/').filter(file => file.endsWith('.js'));
 for (const file of commandfiles) {
@@ -24,57 +25,6 @@ eventFiles.forEach(fEvt => {
 	delete require.cache[require.resolve(`./src/events/${fEvt}`)];
 });
 
-client.on('message', (message) => {
-	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-	const args = message.content.slice(config.prefix.length).trim().split(/ +/);
-	const commandName = args.shift().toLowerCase();
-	const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-	if (!command) return;
-
-	// Arguments
-	if (command.args && !args.length) {
-		let reply = `You didn't provide any arguments, ${message.author}!`;
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${config.prefix}${command.name} ${command.usage}\``;
-			return message.channel.send(reply);
-		}
-	}
-
-	// Cooldown
-	if (!cooldowns.has(command.name)) {
-		cooldowns.set(command.name, new Discord.Collection());
-	}
-
-	const now = Date.now();
-	const timestamps = cooldowns.get(command.name);
-	const cooldownAmount = (command.cooldown || 3) * 1000;
-
-	if (timestamps.has(message.author.id)) {
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-		if (now < expirationTime) {
-			return message.react('⏰');
-		}
-	}
-
-	timestamps.set(message.author.id, now);
-	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
-	// Execution
-	try {
-		command.execute(client, message, args);
-	}
-	catch (error) {
-		console.error(error);
-		return message.channel.send('there was an error trying to execute that command!');
-	}
-
-	// Anti mp
-	if (command.guildOnly && message.channel.type === 'dm') {
-		return message.reply('I can\'t execute that command inside DMs!');
-	}
-
-});
 
 // Login
 client.login(config.token);
---
+++ src/events/message.js
@@ -0,0 +1,62 @@
+const config = require("../config.json");
+const Discord = require("discord.js");
+
+module.exports = async (client, message) => {
+    if (message.webhookID) {
+        if (message.channel.id == "302681957387599872") {
+            if (message.embeds[0]) {
+                if (message.embeds[0].title.includes("New star added")) {
+                    message.react("❤");
+                }
+            } 
+        }
+    }
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    if (!command) return;
+
+    // Arguments
+    if (command.args && !args.length) {
+        let reply = `You didn't provide any arguments, ${message.author}!`;
+        if (command.usage) {
+            reply += `\nThe proper usage would be: \`${config.prefix}${command.name} ${command.usage}\``;
+            return message.lineReply(reply);
+        }
+    }
+
+    // Cooldown
+    if (!client.cooldowns.has(command.name)) {
+        client.cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = client.cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            return message.react('⏰');
+        }
+    }
+
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+    // Execution
+    try {
+        command.execute(client, message, args);
+    }
+    catch (error) {
+        console.error(error);
+        return message.lineReply('THere was an error trying to execute that command!');
+    }
+
+    // Anti mp
+    if (command.guildOnly && message.channel.type === 'dm') {
+        return message.lineReply('I can\'t execute that command inside DMs!');
+    }
+}
