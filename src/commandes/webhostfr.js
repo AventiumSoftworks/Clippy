c6a30a6ff6ad6c39816c2ee6d4c437642c182cff
GeekCornerGH <45696571+GeekCornerGH@users.noreply.github.com>
2023-06-25 12:59:44 +0200

    chore: Remove v14 command and add webhostfr command

---
+++ src/commandes/webhostfr.js
@@ -0,0 +1,5 @@
+module.exports = {
+	title: "Hébergeur non conforme détecté",
+	description: "Vous feriez mieux de changer de fournisseur d'hébergement. Les hébergeurs gratuits empêchent Helios de télécharger les fichiers dans 99% des cas.",
+	fields: []
+}
