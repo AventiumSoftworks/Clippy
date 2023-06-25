40ff3e2791b119b03fed9896f7a40d542d230ff5
GeekCornerGH <45696571+GeekCornerGH@users.noreply.github.com>
2023-06-25 13:41:42 +0200

    feat: Add coremods command

---
+++ src/commandes/coremodsfr.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "title": "Vos coremods ne sont pas chargés ?",
+    "description": "Assurez-vous de mettre vos coremods (par exemple, MalisisCore) dans le dossier Libraries sur Nebula au lieu de les mettre dans le dossier Forgemods. Sinon, ils ne seront pas chargés au lancement du jeu.",
+    "fields": []
+}
