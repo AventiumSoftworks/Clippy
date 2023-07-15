aedccca395bb312cb0ca06a118db5659c62ef514
GeekCorner <45696571+GeekCornerGH@users.noreply.github.com>
2023-07-15 19:59:45 +0200

    fix: Wrong mod name

--- src/commandes/coremods.js
+++ src/commandes/coremods.js
@@ -1,5 +1,5 @@
 module.exports = {
 	"title": "Are your coremods not loaded?",
-	"description": "Make sure to put your coremods (eg. Malisis Doors) in the Libraries folder on Nebula instead of putting them in the Forgemods folder. Else, it won't be loaded at all",
+	"description": "Make sure to put your coremods (eg. MalisisCore) in the Libraries folder on Nebula instead of putting them in the Forgemods folder. Else, it won't be loaded at all",
 	"fields": []
 }
