2e247d1da3a65e41c6d2d9348422f82026b3cb21
GeekCorner <45696571+GeekCornerGH@users.noreply.github.com>
2023-07-16 13:07:46 +0200

    feat: Add mention regex

---
+++ src/commandes/mention.js
@@ -0,0 +1,5 @@
+module.exports = {
+    title: "Please don't just mention users/helpers when requesting help.",
+    description: "* Mentioning people without providing context won't help. Please include context when requesting help.\n* Bumping issues by just mentioning someone won't help, and in most cases, will result in your message being ignored.\n* Please don't DM people regarding your issue. Just keep the issue in this server. Thank you.",
+    fields: []
+}
---
+++ src/commandes/mentionfr.js
@@ -0,0 +1,5 @@
+module.exports = {
+    title: "S'il vous plaît, ne mentionnez pas simplement les utilisateurs/aides lorsque vous demandez de l'aide.",
+    description: "* Mentionner des personnes sans fournir de contexte ne vous aidera pas. Veuillez inclure le contexte lors de votre demande d'aide.\n* Mentionner simplement quelqu'un pour résoudre les problèmes ne vous aidera pas et, dans la plupart des cas, entraînera l'ignorance de votre message.\n* S'il vous plaît, ne contactez pas les gens en MP concernant votre problème. Veuillez garder le problème dans ce serveur. Merci.",
+    fields: []
+}
