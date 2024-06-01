6e201347821600ecba469e193a7c5bc6d44d06ee
GeekCorner <45696571+GeekCornerGH@users.noreply.github.com>
2024-06-01 07:28:26 +0200

    feat: Message logger

---
+++ src/events/messageDelete.js
@@ -0,0 +1,12 @@
+const { MessageEmbed } = require("discord.js");
+const config = require("../config.json");
+
+module.exports = async (client, message) => {
+    if (!message.guild || !config.loggedServers.includes(message.guild.id) || !message.content) return;
+    const embed1 = new MessageEmbed()
+    .setAuthor({ name: `${message.member.displayName} (${message.user.id})`, icon: `${message.member.displayAvatarURL()}` })
+        .setTitle("Message contents")
+        .setColor("Red")
+        .setDescription(message.content);
+    client.channels.cache.get(config.logsChannelId).send({ content: "A message has been deleted", embeds: [embed1] })
+}
---
+++ src/events/messageUpdate.js
@@ -0,0 +1,17 @@
+const { MessageEmbed } = require("discord.js");
+const config = require("../config.json");
+
+module.exports = async (client, oldMessage, newMessage) => {
+    if (!oldMessage.guild || !config.loggedServers.includes(oldMessage.guild.id) || !oldMessage.content || !newMessage.content || oldMessage.content === newMessage.content) return;
+    const embed1 = new MessageEmbed()
+        .setAuthor({ name: `${oldMessage.member.displayName} (${oldMessage.user.id})`, icon: `${oldMessage.member.displayAvatarURL()}` })
+        .setTitle("Old message contents")
+        .setColor("Red")
+        .setDescription(oldMessage.content);
+    const embed2 = new MessageEmbed()
+        .setAuthor({ name: `${oldMessage.member.displayName} (${oldMessage.user.id})`, icon: `${oldMessage.member.displayAvatarURL()}`})
+        .setTitle("New message contents")
+        .setColor("Green")
+        .setDescription(newMessage.content);
+    client.channels.cache.get(config.logsChannelId).send({ content: `A message has been updated (${oldMessage.url})`, embeds: [embed1, embed2] })
+}
