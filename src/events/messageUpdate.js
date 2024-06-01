cd2d2408daafd8a01242e8f699e986c3d61ccb08
GeekCorner <45696571+GeekCornerGH@users.noreply.github.com>
2024-06-01 07:37:22 +0200

    fix: Icons

--- src/events/messageUpdate.js
+++ src/events/messageUpdate.js
@@ -4,12 +4,12 @@ const config = require("../config.json");
 module.exports = async (client, oldMessage, newMessage) => {
     if (!oldMessage.guild || !config.loggedServers.includes(oldMessage.guild.id) || !oldMessage.content || !newMessage.content || oldMessage.content === newMessage.content) return;
     const embed1 = new EmbedBuilder()
-        .setAuthor({ name: `${oldMessage.member.displayName} (${oldMessage.member.id})`, icon: `${oldMessage.member.displayAvatarURL()}` })
+        .setAuthor({ name: `${oldMessage.member.displayName} (${oldMessage.member.id})`, iconURL: `${oldMessage.member.displayAvatarURL()}` })
         .setTitle("Old message contents")
         .setColor("Red")
         .setDescription(oldMessage.content);
     const embed2 = new EmbedBuilder()
-        .setAuthor({ name: `${oldMessage.member.displayName} (${oldMessage.member.id})`, icon: `${oldMessage.member.displayAvatarURL()}`})
+        .setAuthor({ name: `${oldMessage.member.displayName} (${oldMessage.member.id})`, iconURL: `${oldMessage.member.displayAvatarURL()}`})
         .setTitle("New message contents")
         .setColor("Green")
         .setDescription(newMessage.content);
